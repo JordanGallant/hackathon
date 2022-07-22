8db2474398e2ee7f091dcf75c7ebc2a16e581c50
JordanGallant <39-12602@sae.edu.za>
2022-07-22 20:09:18 +0200

    balance

---
+++ src/components/Dasboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Dashboard.css'
+
+function Dashboard() {
+  return (
+    <>
+  <div className='body'>
+   <div className='top-div'> <h1>Emmision Coin</h1></div>
+   <div className='balance'><h1>0</h1> <h5>Balance</h5></div>
+
+  </div>
+    </>
+  );
+}
+
+export default Dashboard;
