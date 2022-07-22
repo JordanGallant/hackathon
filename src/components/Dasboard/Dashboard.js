368f88ea7c4deeba28b5e70b4664f4ebed5d7268
JordanGallant <39-12602@sae.edu.za>
2022-07-22 20:52:19 +0200

    update-buttons

--- src/components/Dasboard/Dashboard.js
+++ src/components/Dasboard/Dashboard.js
@@ -7,6 +7,7 @@ function Dashboard() {
   <div className='body'>
    <div className='top-div'> <h1>Emmision Coin</h1></div>
    <div className='balance'><h1>0</h1> <h5>Balance</h5></div>
+   <div className='button-div'><div className='top-buttons'><button className='claim'>Claim Tokens</button> <button className='send'>Send</button></div><div className='bottom-button'><button className='sell'>Sell</button></div></div>
 
   </div>
     </>
